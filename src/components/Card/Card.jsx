783a6914f5701d6b1fe6ac81ece096b142f54bf2
DaywisonSilva <daywisonsilvasou@gmail.com>
2023-09-02 12:11:23 -0300

    adicionando imagem ao componente

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,16 @@
 import Style from './style.module.css'
+import Icon from '../../assets/images/icon-dice.svg'
 
 const Card = () => {
   return (
     <div className={Style.card}>
+      <p className={Style.frase}>frase #1</p>
       <p className={Style.text}>
         &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         Accusantium voluptatem rerum velit libero, sed voluptatum labore porro
         id.&quot;
       </p>
+      <img src={Icon} alt='Icon' className={Style.icon} />
     </div>
   )
 }
